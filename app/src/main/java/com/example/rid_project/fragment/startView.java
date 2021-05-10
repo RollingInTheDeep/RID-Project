569fae4b46cae25ebd01582f8b9a8dde2f9f2a53
whdpwls2486@naver.com <whdpwls2486@naver.com>
2021-05-10 21:24:28 +0900

    [#3] markup - startview.xml <-> StartView.java

---
+++ app/src/main/java/com/example/rid_project/fragment/startView.java
@@ -0,0 +1,15 @@
+package com.example.rid_project.fragment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.rid_project.R;
+
+public class startView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startview);
+    }
+}
