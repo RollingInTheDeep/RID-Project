cc3a63d8ab711ea44334e89602777f1a5899244e
whdpwls2486@naver.com <whdpwls2486@naver.com>
2021-05-10 21:40:45 +0900

    [#3] markup - startview.PNG -> startView.xml

--- app/src/main/java/com/example/rid_project/fragment/startView.java
+++ app/src/main/java/com/example/rid_project/fragment/startView.java
@@ -6,10 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.rid_project.R;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
 public class startView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startview);
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
