fa786ac907a113b13945a23d9c9c292951cfccc3
Da-yun <ekdbs6963@naver.com>
2021-05-14 14:45:00 +0900

    [#10] login Api - SignInActivity

--- app/src/main/java/com/example/rid_project/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/rid_project/ui/activity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.rid_project.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -9,25 +12,39 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.rid_project.ui.fragment.BottomNavigationFragment;
 import com.example.rid_project.ui.fragment.MainFragment;
 import com.example.rid_project.R;
+import com.example.rid_project.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private FragmentManager fragmentManager;  //  프래그먼트 매니저
+    private ActivityMainBinding binding;
     private BottomNavigationFragment fragmentBottom;  //  Fragment NavBar
     private MainFragment fragmentMain;  //  메인뷰 프래그먼트
+    private String userID;
+    private String userName;
+    private FrameLayout fragmentView;
     private FragmentTransaction transaction;
+    private TextView textViewMain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fragmentManager = getSupportFragmentManager();
-        transaction = fragmentManager.beginTransaction();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        Intent intent = getIntent();  //  이전 signin activity intent
+        userID = intent.getStringExtra("userID");  //  사용자 uid
+        userName = intent.getStringExtra("userName");  //  사용자 이름
 
+        fragmentManager = getSupportFragmentManager();   //액티비티 위에 프래그먼트 연결
+        transaction = fragmentManager.beginTransaction();
         fragmentBottom = new BottomNavigationFragment();
         fragmentMain = new MainFragment();
         transaction.replace(R.id.fragment_main_view, fragmentMain);
         transaction.replace(R.id.fragment_main_navigationbar, fragmentBottom).commit();
 
-    }
+        fragmentView = binding.fragmentMainView;  // 책장 프래그먼트
+        //textViewMain = fragment_view.findViewById(R.id.textView_main_topBar);  // 책장 유저이름 칸
+       // textViewMain.setText(userName + "의 책장");
 
+    }
 }
