514cd480bb34ea3f0956511122134e61d09ed33a
whdpwls2486@naver.com <whdpwls2486@naver.com>
2021-05-15 23:01:01 +0900

    [#3] 컨벤선에 맞게 파일명 등 수정
    InnerTextFragment 파일명 수정
    readview -> ReadTextFragment 파일명 수정
    startview -> StartActivity 파일명 수정
    textlistview -> TextListActivity 파일명 수정
    Activity, Fragment Convention 수정
    
    Date:      Sat May 15 23:01:01 2021 +0900

---
+++ app/src/main/java/com/example/rid_project/fragment/ReadTextFragment.java
@@ -0,0 +1,19 @@
+package com.example.rid_project.fragment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.rid_project.R;
+
+public class ReadTextFragment extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.read_text_fragment);
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
--- app/src/main/java/com/example/rid_project/ui/activity/TextListActivity.java
+++ app/src/main/java/com/example/rid_project/ui/activity/TextListActivity.java
@@ -1,13 +1,19 @@
-package com.example.rid_project.fragment;
+package com.example.rid_project.ui.activity;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.rid_project.R;
 
-public class textListView extends AppCompatActivity {
+
+public class TextListActivity extends AppCompatActivity {
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.textlistview);
+        setContentView(R.layout.activity_text_list);
     }
+
 }
