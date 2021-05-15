bc381fc7a533ea5b95e697bbf70917532d4947d2
whdpwls2486@naver.com <whdpwls2486@naver.com>
2021-05-15 23:01:01 +0900

    [#3] 컨벤선에 맞게 파일명 등 수정
    InnerTextFragment 파일명 수정
    readview -> ReadTextFragment 파일명 수정
    startview -> StartActivity 파일명 수정
    textlistview -> TextListActivity 파일명 수정
    Activity, Fragment Convention 수정
    
    Date:      Sat May 15 23:01:01 2021 +0900
    
    [#16] fix-convention 수정

--- app/src/main/java/com/example/rid_project/ui/activity/TextListActivity.java
+++ app/src/main/java/com/example/rid_project/ui/activity/TextListActivity.java
@@ -1,19 +1,19 @@
-package com.example.rid_project.fragment;
+package com.example.rid_project.ui.activity;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.rid_project.R;
 
-public class ReadView extends AppCompatActivity {
+
+public class TextListActivity extends AppCompatActivity {
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.read);
+        setContentView(R.layout.activity_text_list);
     }
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
+
 }
---
+++ app/src/main/java/com/example/rid_project/ui/fragment/InnerTextFragment.java
@@ -0,0 +1,25 @@
+package com.example.rid_project.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.rid_project.R;
+
+public class InnerTextFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.inner_text_fragment, container, false); //container <-부모 사이즈를 주고 , false=아직 붙이지 않는다.
+
+        return view;
+
+    }
+
+}
