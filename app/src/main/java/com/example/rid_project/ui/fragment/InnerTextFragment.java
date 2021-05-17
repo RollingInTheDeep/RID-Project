3077711c3b78b92fb9a6d53fce4e94474b084a61
OMEGA-Y <science_co@naver.com>
2021-05-17 21:11:33 +0900

    [#20] Project File <-> Camera Api 연동
    [#20] conflict 수정

--- app/src/main/java/com/example/rid_project/ui/fragment/InnerTextFragment.java
+++ app/src/main/java/com/example/rid_project/ui/fragment/InnerTextFragment.java
@@ -1,15 +1,26 @@
 package com.example.rid_project.ui.fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.rid_project.R;
 
-public class InnerTextFragment extends AppCompatActivity {
+public class InnerTextFragment extends Fragment {
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_inner_text);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_inner_text, container, false); //container <-부모 사이즈를 주고 , false=아직 붙이지 않는다.
+
+        return view;
+
     }
+
 }
