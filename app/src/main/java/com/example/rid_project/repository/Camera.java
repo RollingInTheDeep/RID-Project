801ac541daf877be64d568801dcc925facea065a
OMEGA-Y <science_co@naver.com>
2021-05-30 02:10:38 +0900

    [#20] Camera SurfaceView, 촬영가능한 상태까지 구현

---
+++ app/src/main/java/com/example/rid_project/repository/Camera.java
@@ -0,0 +1,3 @@
+package com.example.rid_project.repository;
+
+
--- app/src/main/java/com/example/rid_project/ui/fragment/BottomNavigationFragment.java
+++ app/src/main/java/com/example/rid_project/ui/fragment/BottomNavigationFragment.java
@@ -1,17 +1,17 @@
 package com.example.rid_project.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import com.example.rid_project.R;
 import com.example.rid_project.databinding.FragmentBottomnavigationBinding;
+import com.example.rid_project.ui.activity.CameraActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BottomNavigationFragment extends Fragment {
@@ -21,14 +21,15 @@ public class BottomNavigationFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //보여질 뷰객체를 생성
+    public View onCreateView(@Nullable LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentBottomnavigationBinding.inflate(inflater, container, false);
         bottomNavigationView = binding.bottomNavigationViewMain;
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 item -> {
                     switch (item.getItemId()) {
                         case R.id.cameraItem_navigation:
+                            getActivity().startActivity(new Intent(getActivity(), CameraActivity.class));
+
                             Log.e("dd","d");
                             return true;
                         case R.id.bookitem_navigation:
@@ -41,7 +42,6 @@ public class BottomNavigationFragment extends Fragment {
                 });
 
         return binding.getRoot();
-
     }
 
 }
